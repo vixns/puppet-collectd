b0b6a6d425a44bb49dc449a8184b467c6908f351
Mickaël Canévet <mickael.canevet@camptocamp.com>
2012-12-06 16:13:16 +0100

    Lint collectd::init

--- manifests/init.pp
+++ manifests/init.pp
@@ -2,7 +2,7 @@
 # (C) Copyright: 2008, David Schmitt <david@dasz.at>
 
 # Module: collectd
-# 
+#
 # To start managing the collectd, include the collectd class in your node. Use
 # the collectd::conf define to set basic parameters of your installation.
 # collectd::plugin is the foundation of many plugin-specific defines which help
@@ -13,80 +13,82 @@
 # /etc/collectd/collectd.conf file.
 class collectd {
 
-  file {
-    "/var/lib/puppet/modules":
-      ensure => directory,
-      recurse => true, purge => true, force => true,
-      mode => 0755, owner => root, group => 0;
+  file {'/var/lib/puppet/modules':
+    ensure  => directory,
+    recurse => true,
+    purge   => true,
+    force   => true,
+    mode    => '0755',
+    owner   => root,
+    group   => 0;
   }
 
-	collectd::libdir { ['collectd', 'collectd/plugins', 'collectd/thresholds' ]: }
+  collectd::libdir { ['collectd', 'collectd/plugins', 'collectd/thresholds' ]: }
 
-	package {
-		'collectd':
-			ensure => installed;
-	}
+  package {'collectd':
+    ensure => installed;
+  }
 
-	service {
-		'collectd':
-			ensure => running,
-			enable => true,
-			hasrestart => true,
-			pattern => collectd,
-			require => Package['collectd'];
-	}
+  service {'collectd':
+    ensure     => running,
+    enable     => true,
+    hasrestart => true,
+    pattern    => collectd,
+    require    => Package['collectd'];
+  }
 
-	file {
-		'/etc/collectd/':
-			ensure => 'directory',
-			mode => 0755, owner => root, group => 0,
-			require => Package['collectd'],
-	}
+  file {'/etc/collectd/':
+    ensure  => 'directory',
+    mode    => '0755',
+    owner   => root,
+    group   => 0,
+    require => Package['collectd'],
+  }
 
-	file {
-		'/etc/collectd/collectd.conf':
-			ensure => present,
-			mode => 0644, owner => root, group => 0,
-			require => [Package['collectd'], File['/etc/collectd/']],
-			notify => Service['collectd'];
-	}
+  file {'/etc/collectd/collectd.conf':
+    ensure  => present,
+    mode    => '0644',
+    owner   => root,
+    group   => 0,
+    require => [Package['collectd'], File['/etc/collectd/']],
+    notify  => Service['collectd'];
+  }
 
-	collectd::conf {
-		'Include':
-			value => "/var/lib/puppet/modules/collectd/plugins/*.conf";
-	}
+  collectd::conf {'Include':
+    value => '/var/lib/puppet/modules/collectd/plugins/*.conf';
+  }
 
-	# add customisations for distributions here
-	case $operatingsystem {
-		'debian': {
-			case $debianversion {
-				'etch': {
-				}
-			}
-		}
-		'redhat': {
-			file {
-				'/etc/collectd.conf':
-					ensure => 'link',
-					target => '/etc/collectd/collectd.conf',
-					require => File['/etc/collectd/collectd.conf'],
-					notify => Service['collectd'];
-			}
-      case $lsbmajdistrelease {
-        "4","5": { }
+  # add customisations for distributions here
+  case $::operatingsystem {
+    'debian': {
+      case $::debianversion {
+        'etch': {
+        }
+      }
+    }
+    'redhat': {
+      file {
+        '/etc/collectd.conf':
+          ensure  => 'link',
+          target  => '/etc/collectd/collectd.conf',
+          require => File['/etc/collectd/collectd.conf'],
+          notify  => Service['collectd'];
+      }
+      case $::lsbmajdistrelease {
+        '4','5': { }
         default: {
-          file{ ["/usr/bin/collectd-nagios"]:
-            mode    => "755",
-            owner   => "root",
-            group   => "root",
-            seltype => "nagios_services_plugin_exec_t",
+          file{ ['/usr/bin/collectd-nagios']:
+            mode    => '0755',
+            owner   => 'root',
+            group   => 'root',
+            seltype => 'nagios_services_plugin_exec_t',
             require => Package['collectd'];
           }
         }
       }
-		}
-		default: {
-			# no changes needed
-		}
-	}
+    }
+    default: {
+      # no changes needed
+    }
+  }
 }
