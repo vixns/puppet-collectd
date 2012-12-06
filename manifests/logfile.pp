1bc2241cad02ef8b788dfbb5740282d81d32b19d
Mickaël Canévet <mickael.canevet@camptocamp.com>
2012-12-06 16:18:11 +0100

    Lint collectd::logfile

--- manifests/logfile.pp
+++ manifests/logfile.pp
@@ -3,19 +3,21 @@
 
 # Define: collectd::logfile
 # specifies a logfile for collectd
-# 
+#
 # Parameters:
-#   namevar	- The basename for the logfile. It'll actually be called "/var/log/collectd_${namevar}.log"
-#   level	- The collectd loglevel
-#   timestamp	- 'true' (default) or 'false'; whether to add timestamps to the log
-define collectd::logfile($level, $timestamp = 'true') {
-	collectd::plugin {
-		"logfile_${name}":
-			lines => [
-				"File \"/var/log/collectd_${name}.log\"",
-				"Timestamp ${timestamp}",
-				"LogLevel ${level}"
-			]
-	}
+#   namevar   - The basename for the logfile. It'll actually be called
+#               "/var/log/collectd_${namevar}.log"
+#   level     - The collectd loglevel
+#   timestamp - 'true' (default) or 'false'; whether to add timestamps to the
+#               log
+define collectd::logfile($level, $timestamp = true) {
+  collectd::plugin {
+    "logfile_${name}":
+      lines => [
+        "File \"/var/log/collectd_${name}.log\"",
+        "Timestamp ${timestamp}",
+        "LogLevel ${level}"
+      ]
+  }
 }
 
