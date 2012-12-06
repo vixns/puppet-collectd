c30ab08a101bf8b3a395d392c7c692b31ac9edd6
Mickaël Canévet <mickael.canevet@camptocamp.com>
2012-12-06 16:22:23 +0100

    Lint example

--- doc/example.pp
+++ doc/example.pp
@@ -4,30 +4,30 @@ filebucket { 'server': }
 
 include collectd
 
-resources { collectd_conf: purge => true; }
+resources { 'collectd_conf': purge => true; }
 
 collectd::conf {
-	'FQDNLookup':
-		value => 'true';
-	'Server':
-		value => [ '"foo" 1002', '"foo3" 2000' ];
-	'LoadPlugin':
-		value => [ 'syslog', 'network', 'cpu' ];
+  'FQDNLookup':
+    value => true;
+  'Server':
+    value => [ '"foo" 1002', '"foo3" 2000' ];
+  'LoadPlugin':
+    value => [ 'syslog', 'network', 'cpu' ];
 }
 
 collectd::syslog { 'debug': }
 collectd::logfile {
-	'debug': level => debug;
-	'error': level => err;
+  'debug': level => debug;
+  'error': level => err;
 }
 
 collectd_threshold {
     '*/*:bar/foo':
-        warning_min => "2.00",
-        warning_max => "1000.00",
-        failure_min => "0.00",
-        failure_max => "1200.00",
-        invert => false;
+        warning_min => '2.00',
+        warning_max => '1000.00',
+        failure_min => '0.00',
+        failure_max => '1200.00',
+        invert      => false;
     '*/interface:eth0/if_octets:rx':
         failure_max => 10000000;
     'hostname/*:idle/cpu':
